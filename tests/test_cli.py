bb5f85fcafc05a8268d07d3da3ca238ba108d6ae
Anton1 <loukik56@mail.ru>
2021-06-28 10:27:02 +0300

    File LICENSE added.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,7 +6,7 @@ import argparse
 @mock.patch('argparse.ArgumentParser.parse_args',
             return_value=argparse.Namespace(file=["hello"], text=None))
 def test_parsing_file(mock_args):
-    result = cli.cli()
+    result = cli()
     assert result == "file: 3"
 
 
@@ -14,19 +14,19 @@ def test_parsing_file(mock_args):
             return_value=argparse.Namespace(file=['random', 'character',
                                                   'set'], text=None))
 def test_parsing_file_3_lines(mock_args):
-    result = cli.cli()
+    result = cli()
     assert result == 'file: 6 3 3'
 
 
 @mock.patch('argparse.ArgumentParser.parse_args',
             return_value=argparse.Namespace(text='hello', file=None))
 def test_parsing_string(mock_args):
-    result = cli.cli()
+    result = cli()
     assert result == 'text: 3'
 
 
 @mock.patch('argparse.ArgumentParser.parse_args',
             return_value=argparse.Namespace(file=['tested'], text='value'))
 def test_parsing_file_and_string(mock_args):
-    result = cli.cli()
+    result = cli()
     assert result == 'file: 2 text: 5'
