107bb755d80783ba5f2efce3421b3ec580b601ed
Anton1 <loukik56@mail.ru>
2021-06-28 10:37:30 +0300

    Changed the url in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='python package',
     version='1.0',
-    url='https://github.com/Anton45creator/Pathon_Package.git',
-    license='Apache 2.0',
-    author='ThinkPad',
+    url='https://git.foxminded.com.ua/Anton1/task5_python-package.git',
+    author='Anton Viazovik',
     author_email='Loukik56@mail.ru',
     description='A program that counts unique characters in a string.',
 
