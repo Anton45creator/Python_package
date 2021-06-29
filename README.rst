eaa3e5683599cb145ba6638c35037e20a18dafa4
Anton1 <loukik56@mail.ru>
2021-06-29 08:51:45 +0300

    File changed setup.py.

--- README.rst
+++ README.rst
@@ -1,10 +1,13 @@
-Readme
-=========
-
+Python Package
+=======
+This is a command-line program that opens text
+files using the command line and counts the number
+of unique characters in it. It also counts the
+number of characters in the entered string.
 ------------------------------------------------------
 
-1.0
----
+1.0.0
+-----
 
 Changes:
 ~~~~~~~~
--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README", 'r') as f:
+    long_description = f.read()
 
 setup(
     name='task5_python_package',
-    version='1.0.0',
+    version='0.1.0',
     url='https://git.foxminded.com.ua/Anton1/task5_python-package.git',
-    license='',
+    license='proprietary',
     author='ThinkPad',
     author_email='Loukik56@mail.py',
     description='A program that counts unique characters in a string.',
+
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
 
 )
