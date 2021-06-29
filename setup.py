4ac8e5f6b0ba001ea0d6f9a1a76c4424a5971001
Anton1 <loukik56@mail.ru>
2021-06-29 07:20:59 +0300

    The project structure has been changed.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='task5_python_package',
+    version='1.0.0',
+    url='https://git.foxminded.com.ua/Anton1/task5_python-package.git',
+    license='',
+    author='ThinkPad',
+    author_email='Loukik56@mail.py',
+    description='A program that counts unique characters in a string.',
+
+)
